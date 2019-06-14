9dae455af0ff6647682881290dc9b919d7ceefaf
pliao85 <paul.j.liao@gmail.com>
2019-06-14 05:12:13 -0700

    Beta
    for code review

--- guessing_game.py
+++ guessing_game.py
@@ -28,43 +28,56 @@ def start_game():
     
     # Create a variable for number of tries
     attempt = 0
-
-    # Display welcome message 
-    print("""
-          ----------------------------------------
-            Welcome to the Number Guessing Game!
-          ----------------------------------------                   
-          """)
-
-    print_highscore()
             
     # Prompt user for a number between 1 - 10
     while True:
         guess = input("Pick a number between 1 - 10: ")
-        guess = int(guess)
-        if guess < answer:
-            print("It is higher!")
-            attempt += 1
-            print("{}".format(attempt))
-        elif guess > answer:
-            print("It is lower!")
-            attempt += 1
-            print("{}".format(attempt))
-        elif guess == answer:
-            print("You've got it!")
-            attempt += 1
-            print("You have made {} attempt/s.".format(attempt))
-            highscore.append(attempt)
-            highscore.sort()
-            break
+        
+        try:
+            guess = int(guess)            
+            if guess > 10:
+                raise ValueError("Enter a number less than 10")            
+        
+            if guess < answer:
+                print("It is higher!")
+                attempt += 1
+                print("{}".format(attempt))
+            elif guess > answer:
+                print("It is lower!")
+                attempt += 1
+                print("{}".format(attempt))
+            elif guess == answer:
+                print("You've got it!")
+                attempt += 1
+                print("You have made {} attempt/s.".format(attempt))
+                highscore.append(attempt)
+                highscore.sort()
+                break
+            
+        except ValueError as err:
+            print("Oh no we ran into an issue. Please try agian.")
+            if err:
+                print("({})".format(err))          
         
 
 # Create highscore list to hold of the high scores
 highscore = []
 
+# Display welcome message 
+print("""
+      ----------------------------------------
+        Welcome to the Number Guessing Game!
+      ----------------------------------------                   
+      """)
+
+print_highscore()
+
 # Ask the player if you want to play the game again
 while True:
+
     start_game()
+    
+    print_highscore()
     
     retry = input("Would you like to start again? [Y/N] ")
     retry = retry.lower()
